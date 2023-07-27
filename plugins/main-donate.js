eac624549f34a20ebed95da46cbe5f580c68d9bf
Rakrohaku9534 <Rakrohaku9534@github.com>
2023-07-27 12:39:05 +0700

    main

---
+++ plugins/main-donate.js
@@ -0,0 +1,19 @@
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+
+    let don = `
+≡ *DONACION*
+puedes donar si quieres ayudar a mantener el bot activo
+
+▢ *DANA*
+• *Nomor :* 085786539008 
+
+let img = 'https://telegra.ph/file/7361a7ea268d54ad27cee.jpg'
+conn.sendFile(m.chat, img, 'img.jpg', don, m)
+
+}
+handler.help = ['donate']
+handler.tags = ['main']
+handler.command = ['apoyar', 'donate', 'donar'] 
+
+export default handler
